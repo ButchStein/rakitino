38468e51a0fd1f837d9a998ef3aca9681c54d711
ButchStein <butchbs1982@gmail.com>
2019-06-04 13:22:36 +0300

    update

--- source/js/script.js
+++ source/js/script.js
@@ -8,18 +8,54 @@ elem.addEventListener('click', function (e) {
 	}
 });
 
-let scrollUp = document.querySelector('.section-seven__button');
-let t;
+// window.onload = function () {
+// 	let scrolled;
+// 	let timer;
 
-scrollUp.addEventListener('click', function () {
-	scrolltop();
-	function scrolltop() {
-		let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-		if (top > 0) {
-			window.scrollTo(0, -20);
-			t = setTimeout("scrolltop()", 30);
-		} else {
-			clearTimeout(t);
-		} return false;
-	};
+// 	document.querySelector('.section-seven__button').onclick = function () {
+// 		scrolled = window.pageYOffset;
+// 		scrollToTop();
+// 	}
+// 	function scrollToTop() {
+// 		if (scrolled > 0) {
+// 			window.scrollTo(0, scrolled);
+// 			scrolled = scrolled - 2;
+// 			timer = setTimeout(scrollToTop, 10);
+// 		} else {
+// 			clearTimeout(timer);
+// 			window.scrollTo(0, 0);
+// 		}
+// 	}
+// }
+// window.onload = function () {
+// 	document.querySelector('.section-seven__button').onclick = function () {
+		
+
+// 	}
+// 	var scrollAnimate = function (to, duration) {
+// 		if (duration <= 0) return;
+// 		var difference = to - window.pageYOffset;
+// 		var perTick = (difference / duration) * 10;
+
+// 		setTimeout(function () {
+// 			window.scrollTo(0, window.pageYOffset + perTick);
+// 			if (window.pageYOffset === to) return;
+// 			scrollAnimate(to, duration - 10);
+// 		}, 10);
+// 	}
+// }
+
+$(document).ready(function(){
+	$('.section-seven__button').on('click', function(event) {
+
+    var target = $( $(this) );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+
+});
 });
